95c5df507b3ecfb73c35cdf6c631831c46182b60
Rupesh Malpani <Rupesh.Malpani@skillsoft.com>
2020-02-21 07:28:33 +0530

    changes related to search bar functionality

--- source/src/components/App.js
+++ source/src/components/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/Youtube'
 
 class App extends React.Component {
+    state = {
+        videos: []
+    }
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params:{
+                q: term
+            }
+        });
+        this.setState({
+            videos: response.data.items
+        });
+        // console.log(response.data.items);
+    };
+
     render() {
         return (
-            <div>
-                <div><h1>React - YouTube Browser Application</h1></div>
+            <div className="ui container">
+                <div>
+                    <SearchBar onFormSubmit={this.onTermSubmit} />
+                    I have { this.state.videos.length } videos
+                </div>
             </div>
         )
     }
